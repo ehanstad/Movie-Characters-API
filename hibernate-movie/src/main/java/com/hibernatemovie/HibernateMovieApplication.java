2bf7043d195e397e49465c0ad38ef8929e6f92bf
Erik <erik_hanstad@hotmail.com>
2023-02-24 10:29:14 +0100

    Init commit, create maven spring hibernate poject

---
+++ hibernate-movie/src/main/java/com/hibernatemovie/HibernateMovieApplication.java
@@ -0,0 +1,13 @@
+package com.hibernatemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateMovieApplication.class, args);
+	}
+
+}
---
+++ hibernate-movie/src/test/java/com/hibernatemovie/HibernateMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernatemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
